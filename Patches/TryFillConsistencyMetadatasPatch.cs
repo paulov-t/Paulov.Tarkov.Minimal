0773133af753c0a529744a0bdc53a7a9e0a13656
Paulov-t <pargsoft@hotmail.com>
2025-05-03 08:58:32 +0100

    Fix incorrect flags in GetMethodToPatch

--- Patches/TryFillConsistencyMetadatasPatch.cs
+++ Patches/TryFillConsistencyMetadatasPatch.cs
@@ -1,26 +1,36 @@
+using FilesChecker;
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
-using FilesChecker;
-using HarmonyLib;
 
 namespace Paulov.Tarkov.Minimal.Patches;
 
 public class TryFillConsistencyMetadatasPatch : IPaulovHarmonyPatch
 {
+    private static Type GetConsistencyControllerType()
+    {
+        Type[] fileCheckerTypes = Assembly.GetAssembly(typeof(FilesCheckerFactory)).GetTypes();
+        for (var i = 0; i < fileCheckerTypes.Length; i++)
+        {
+            Plugin.Logger.LogDebug($"{nameof(TryFillConsistencyMetadatasPatch)}.{nameof(GetConsistencyControllerType)}: {fileCheckerTypes[i].Name}");
+        }
+        return fileCheckerTypes.Single(x => x.Name == "ConsistencyController");
+    }
+
     public MethodBase GetMethodToPatch()
     {
-        const BindingFlags flags = BindingFlags.Public | BindingFlags.Instance;
+        const BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Instance;
         MethodInfo[] controllerMethods = GetConsistencyControllerType().GetMethods(flags);
         MethodInfo method = controllerMethods.Single(x => x.Name == "TryFillConsistencyMetadatas");
-        
+
         Plugin.Logger.LogDebug($"{nameof(TryFillConsistencyMetadatasPatch)}.{nameof(GetMethodToPatch)}:{method.Name}");
-        
+
         return method;
     }
-    
+
     public HarmonyMethod GetTranspilerMethod()
     {
         return new HarmonyMethod(this.GetType().GetMethod(nameof(TranspilerMethod), BindingFlags.Public | BindingFlags.Static));
@@ -36,21 +46,17 @@ public class TryFillConsistencyMetadatasPatch : IPaulovHarmonyPatch
             new(OpCodes.Callvirt, AccessTools.Method(typeof(Dictionary<string, FileConsistencyMetadata>), nameof(Dictionary<string, FileConsistencyMetadata>.Clear))),
             new(OpCodes.Ret)
         ];
-        
+
         return codeInstructions;
     }
 
-    private static Type GetConsistencyControllerType()
-    {
-        Type[] fileCheckerTypes = Assembly.GetAssembly(typeof(FilesCheckerFactory)).GetTypes();
-        return fileCheckerTypes.Single(x => x.Name == "ConsistencyController");
-    }
+
 
     public HarmonyMethod GetPrefixMethod()
     {
         return null;
     }
-    
+
     public HarmonyMethod GetPostfixMethod()
     {
         return null;
